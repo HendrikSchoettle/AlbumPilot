67d707f7168586da5c410f92573c22c5384ef7aa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-20 12:56:54 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['AlbumPilot_title'] = 'AlbumPilot - Sincronización automatizada';
+$lang['Album_search_placeholder'] = 'Buscar álbumes...';
+$lang['Albums_to_sync'] = 'Álbumes para sincronizar';
+$lang['External_trigger_description'] = 'Este enlace puede utilizarse en un script para ejecutar AlbumPilot externamente. Inicie en Windows usando Chrome con: start "" "C:\Program Files\Google\Chrome\Application\Chrome.exe --new-window --autoplay-policy=no-user-gesture-required --disable-blink-features=AutomationControlled --disable-popup-blocking --disable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure --disable-background-timer-throttling --disable-renderer-backgrounding --disable-infobars "https://..."';
+$lang['External_trigger_url'] = 'URL de lanzamiento externo';
+$lang['Include_subalbums'] = 'Buscar en sub-álbumes';
+$lang['Options_heading'] = 'Opciones';
+$lang['Reset_settings'] = 'Restaurar valores';
+$lang['Select_all_steps'] = 'Seleccionar/deseleccionar todo';
+$lang['Simulate_mode'] = 'Ejecutar sólo como simulación';
+$lang['Start_sync'] = 'Iniciar la sincronización';
+$lang['VideoJS_AddPoster'] = 'Generar póster a partir del fotograma en';
+$lang['VideoJS_AddThumb'] = 'Generar miniaturas automáticamente cada';
+$lang['VideoJS_OutputFormat'] = 'Formato de salida para el póster';
+$lang['VideoJS_OverlayAdd'] = 'Aplicar efecto de película al póster';
+$lang['VideoJS_PosterOverwrite'] = 'Sobreescribir el póster existente (si lo hay)';
+$lang['VideoJS_PosterSec'] = 'segundos';
+$lang['VideoJS_RepAdd'] = 'Utilizar un póster subido (si está disponible)';
+$lang['VideoJS_ThumbSec'] = 'segundos';
+$lang['VideoJS_ThumbSize'] = 'Tamaño de la miniatura';
+$lang['VideoJS_jpg'] = 'JPG';
+$lang['VideoJS_png'] = 'PNG';
+$lang['all_albums_label'] = 'Álbumes';
+$lang['all_steps_completed'] = 'Completados todos los pasos';
+$lang['batch_mode_limited_ui'] = 'La interfaz de usuari se ha inhabilitado porque el proceso se ha lanzado externamente.';
+$lang['batch_mode_warning'] = 'Modo por lotes activo';
+$lang['documentation_link'] = 'Documentación de AlbumPilot';
+$lang['error_during_step'] = 'Error durante el paso';
+$lang['file_label'] = 'Archivo';
+$lang['image_id'] = 'ID de imagen';
+$lang['include_subalbums'] = 'Incluir sub-álbumes';
+$lang['invalid_response'] = '❌ Respuesta no válida (JSON no válido):';
+$lang['label_all_files'] = 'todos los archivos';
+$lang['label_live'] = 'En vivo';
+$lang['label_only_new'] = 'sólo archivos nuevos';
+$lang['label_select_thumb_types'] = 'Tamaños de miniaturas';
+$lang['label_simulate'] = 'Simulación';
+$lang['label_subalbums_no'] = 'sólo el álbum principal';
+$lang['label_subalbums_yes'] = 'incluyendo los sub-álbumes';
+$lang['label_thumb_overwrite'] = 'Sobreescribir las miniaturas existentes (si existen)';
+$lang['leave_warning'] = 'Hay una sincronización todavía en proceso. ¿Seguro que desea dejar la página?';
+$lang['log_derivative_error'] = 'Error derivativo para el ID %d (%s): %s';
+$lang['log_file_missing'] = 'Falta el archivo para el ID %d (%s) - Archivo no encontrado';
+$lang['log_get_target_size_error'] = 'No se ha podido obtener el tamaño de destino (tipo: %s) - ID %d (%s): %s';
+$lang['log_image_too_small'] = 'Demasiado pequeño para %s - ID %d (%s): Original %dx%d, necesario ≥ %dx%d';
+$lang['log_invalid_dimensions'] = 'Dimensiones de la imagen en la BD no válidas para el ID %d (%s) - falta el ancho/alto';
+$lang['log_md5_calc_error'] = 'Error al calcular la suma de comprobación MD5: %s';
+$lang['log_md5_file_missing'] = 'Archivo no encontrado: %s';
+$lang['log_md5_no_album'] = 'No se ha seleccionado ningún álbum válido';
+$lang['log_md5_progress_line'] = 'Suma de comprobación %d de %d (%d%%) – ID de imagen %d%s | Vía de acceso: %s';
