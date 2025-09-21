7de6fc64d4f5d30c1550732b608aee84000c7bcc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-21 17:56:48 +0200

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['AlbumPilot_title'] = 'AlbumPilot – Sincronització automàtica';
+$lang['Album_search_placeholder'] = 'Cerca àlbums...';
+$lang['Albums_to_sync'] = 'Àlbums per sincronitzar';
