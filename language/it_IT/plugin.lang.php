ab09e70aa70560b8a6e8abc08920badcde5de30a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-29 07:34:04 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['no'] = 'No';
+$lang['progress_heading'] = 'Progressione';
+$lang['selected_album'] = 'Album';
+$lang['simulate_mode'] = 'Simulare';
+$lang['simulation_suffix'] = '(Simulazione)';
+$lang['step_checksum'] = 'immagini';
+$lang['step_metadata'] = 'immagini';
+$lang['step_thumbnail'] = 'miniature';
+$lang['step_update_album_metadata'] = '7. Aggiornamento dei metadati dell\'album';
+$lang['step_video'] = 'video';
+$lang['thumb_type_label'] = 'Tipo';
+$lang['yes'] = 'Si';
+$lang['AlbumPilot_title'] = 'AlbumPilot - Sincronizzazione automatizzata';
+$lang['Album_search_placeholder'] = 'Ricerca album...';
+$lang['Albums_to_sync'] = 'Album da sincronizzare';
+$lang['External_trigger_description'] = 'Questo link può essere utilizzato in uno script per avviare AlbumPilot esternamente. Avviare su Windows utilizzando Chrome con: start "" "C:\NProgram Files\NGoogle\NChrome\NApplication\Nchrome. exe" --new-window --autoplay-policy=no-user-gesture-required --disable-blink-features=AutomationControlled --disable-popup-blocking --disable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure --disable-background-timer-throttling --disable-renderer-backgrounding --disable-infobars "https://...';
+$lang['External_trigger_url'] = 'URL trigger esterno';
+$lang['Include_subalbums'] = 'Cerca nei subalbum';
+$lang['Options_heading'] = 'Opzioni';
+$lang['Reset_settings'] = 'Ripristina impostazioni';
+$lang['Select_all_steps'] = 'Seleziona/Deseleziona tutto';
+$lang['Simulate_mode'] = 'Esegui solo come simulazione';
+$lang['Start_sync'] = 'Avvia la sincronizzazione';
+$lang['VideoJS_AddPoster'] = 'Genera una miniatura dall\'immagine a';
+$lang['VideoJS_AddThumb'] = 'Genera delle miniature automaticamente ogni';
+$lang['VideoJS_OutputFormat'] = 'Formato di output per la miniatura';
+$lang['VideoJS_OverlayAdd'] = 'Applicare un effetto pellicola alla miniatura';
+$lang['VideoJS_PosterOverwrite'] = 'Sostituisci la miniatura esistente (se esiste)';
+$lang['VideoJS_PosterSec'] = 'secondi';
+$lang['VideoJS_RepAdd'] = 'Utilizzare la miniatura caricata (se disponibile)';
+$lang['VideoJS_ThumbSec'] = 'secondi';
+$lang['VideoJS_ThumbSize'] = 'Dimensione miniatura';
+$lang['VideoJS_jpg'] = 'JPG';
+$lang['VideoJS_png'] = 'PNG';
+$lang['all_albums_label'] = 'Album';
+$lang['all_steps_completed'] = 'Tutti i passaggi sono stati completati';
+$lang['batch_mode_limited_ui'] = 'L\'interfaccia utente è disabilitata perché il processo è stato attivato dall\'esterno.';
+$lang['batch_mode_warning'] = 'Modalità batch abilitata';
+$lang['documentation_link'] = 'Documentazione di AlbumPilot';
+$lang['error_during_step'] = 'Errore durante il passo';
+$lang['file_label'] = 'File';
