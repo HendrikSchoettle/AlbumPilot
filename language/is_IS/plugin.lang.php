7149930896ea1574cfc326a5dae73321bf8d9773
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-21 13:28:28 +0200

    Update is_IS, thanks to sveinki

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['workflow_finished'] = 'Vinnuferli lokið';
+$lang['yes'] = 'Já';
+$lang['smartalbums_not_active'] = 'SmartAlbums ekki virkt';
+$lang['step_calculate_checksums'] = '5. Reikna gátsummur sem vantar';
+$lang['step_checksum'] = 'myndir';
+$lang['step_metadata'] = 'myndir';
+$lang['step_thumbnail'] = 'smámyndir';
+$lang['step_video'] = 'myndskeið';
+$lang['sync_in_progress'] = 'Samstilling í gangi...';
+$lang['thumb_type_label'] = 'Tegund';
+$lang['videojs_not_active'] = 'VideoJS ekki virkt';
+$lang['log_write_error_path'] = 'Slóð á atvikaskrá: %s';
+$lang['network_error'] = '❌ Villa í netkerfi:';
+$lang['no'] = 'Nei';
+$lang['of'] = 'af';
+$lang['progress_heading'] = 'Framvinda';
+$lang['select_album_alert'] = 'Veldu albúm.';
+$lang['selected_album'] = 'Albúm';
+$lang['simulate_mode'] = 'Herma';
+$lang['simulation_suffix'] = '(herming)';
+$lang['log_sync_options'] = 'Valkostir';
+$lang['log_sync_step1_options'] = 'Valkostir: %s, %s, %s';
+$lang['log_video_error_details'] = 'Nánar um villu: %s';
+$lang['VideoJS_jpg'] = 'JPG';
+$lang['VideoJS_png'] = 'PNG';
+$lang['all_albums_label'] = 'Albúm';
+$lang['file_label'] = 'Skrá';
+$lang['image_id'] = 'Auðkenni myndar';
+$lang['include_subalbums'] = 'Hafa með undiralbúm';
+$lang['invalid_response'] = '❌ Ógilt svar (ekki gilt JSON):';
+$lang['label_all_files'] = 'allar skrár';
+$lang['label_live'] = 'Í beinni';
+$lang['label_only_new'] = 'aðeins nýjar skrár';
+$lang['label_select_thumb_types'] = 'Stærðir smámynda';
+$lang['label_simulate'] = 'Herma';
+$lang['label_subalbums_no'] = 'einungis albúm';
+$lang['label_subalbums_yes'] = 'undiralbúm meðtalin';
+$lang['Album_search_placeholder'] = 'Leita í albúmum …';
+$lang['Albums_to_sync'] = 'Albúm sem á að samstilla';
+$lang['Include_subalbums'] = 'Leita í undiralbúmum';
+$lang['Options_heading'] = 'Valkostir';
+$lang['Reset_settings'] = 'Frumstilla';
+$lang['Start_sync'] = 'Hefja samstillingu';
+$lang['VideoJS_PosterSec'] = 'sekúndur';
+$lang['VideoJS_ThumbSec'] = 'sekúndur';
+$lang['VideoJS_ThumbSize'] = 'Stærð smámyndar';
