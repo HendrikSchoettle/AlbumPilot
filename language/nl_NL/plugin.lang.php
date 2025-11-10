6dc36517e9148c305c8dfd6fea0d4266467a0ac3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-10 06:43:06 +0100

    Update nl_NL, thanks to pauldaytona

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['External_trigger_description'] = 'Deze link kan worden gebruikt in een script om AlbumPilot extern te starten. Start op Windows met Chrome met: start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --new-window --autoplay-policy=no-user-gesture-required --disable-blink-features=AutomationControlled --disable-popup-blocking --disable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure --disable-background-timer-throttling --disable-renderer-backgrounding --disable-infobars "https://..."';
+$lang['Album_search_placeholder'] = 'Doorzoek albums';
+$lang['Options_heading'] = 'Opties';
+$lang['yes'] = 'Ja';
